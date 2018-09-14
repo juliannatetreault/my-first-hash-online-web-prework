85d794df23f59c0f4f6e38ce078a1eab3afb22e8
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-14 09:09:03 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,6 +15,8 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+   shipping_manifest["oil paintings"]
+   
 end
 
 def adding
@@ -24,7 +26,6 @@ def adding
                   "oil paintings" => 3
                  }
                  
- shipping_manifest["oil paintings"]
   # add 2 muskets to the shipping_manifest hash below
   shipping_manifest["muskets"] = 2
 
