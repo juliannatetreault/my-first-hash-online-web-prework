3783a6178e83c6c648c8363808203eee5e6077d6
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-14 09:05:23 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,7 +25,7 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
   shipping_manifest = ["muskets"] = 2
-
+end
 
   # add 4 gun powder to the shipping_manifest hash below
   shipping_manifest = ["gun powder"] = 4
